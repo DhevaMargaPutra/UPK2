fc5525796aa83a2bf08c9724ca5014abb2b0046b
DhevaMargaPutra <dhevamp57@gmail.com>
2019-01-30 14:35:40 +0700

    mainmenu02

--- mainmenu.js
+++ mainmenu.js
@@ -1,12 +1,49 @@
 const openingDOM = document.getElementById('opening');
-const captureFxDOM = document.getElementById('capture-fx');
-const logoContainerDOM = document.getElementById('logo-container');
+const materiBtnDOM = document.getElementById('materi-button');
+const soalBtnDOM = document.getElementById('soal-button');
+const simulasiBtnDOM = document.getElementById('simulasi-button');
+const profilBtnDOM = document.getElementById('profil-button');
 
 function playCaptureFx () {
-    captureFxDOM.style.animationName = 'capture-fx';
-    logoContainerDOM.style.display = 'none';
+    openingDOM.children[0].style.animationName = 'capture-fx';
 }
 
-captureFxDOM.addEventListener('animationend', function() {
+openingDOM.children[0].addEventListener('animationend', function() {
     openingDOM.style.display = 'none';
-}, false);
+});
+
+// materi
+materiBtnDOM.addEventListener('mouseenter', function() {
+    materiBtnDOM.children[1].style.opacity = '1';
+});
+
+materiBtnDOM.addEventListener('mouseleave', function() {
+    materiBtnDOM.children[1].style.opacity = '0';
+});
+
+// soal
+soalBtnDOM.addEventListener('mouseenter', function() {
+    soalBtnDOM.children[1].style.opacity = '1';
+});
+
+soalBtnDOM.addEventListener('mouseleave', function() {
+    soalBtnDOM.children[1].style.opacity = '0';
+});
+
+// simulasi
+simulasiBtnDOM.addEventListener('mouseenter', function() {
+    simulasiBtnDOM.children[1].style.opacity = '1';
+});
+
+simulasiBtnDOM.addEventListener('mouseleave', function() {
+    simulasiBtnDOM.children[1].style.opacity = '0';
+});
+
+// profil
+profilBtnDOM.addEventListener('mouseenter', function() {
+    profilBtnDOM.children[1].style.opacity = '1';
+});
+
+profilBtnDOM.addEventListener('mouseleave', function() {
+    profilBtnDOM.children[1].style.opacity = '0';
+});
